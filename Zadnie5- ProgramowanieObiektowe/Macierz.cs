c686e88252fbfe24781c6b012b8e7f51f0e224bb
Malgorzata Rywalska <gosia.rywalska@gmail.com>
2024-06-29 15:10:48 +0200

    final commit

---
+++ Zadnie5- ProgramowanieObiektowe/Macierz.cs
@@ -0,0 +1,87 @@
+﻿namespace Zadnie5__ProgramowanieObiektowe;
+
+ public class Macierz<T> : IEquatable<Macierz<T>>
+    {
+        private T[,] tablica;
+
+        public int WymiarWierszy { get; private set; }
+        public int WymiarKolumn { get; private set; }
+
+        public Macierz(int wymiarWierszy, int wymiarKolumn)
+        {
+            if (wymiarWierszy <= 0 || wymiarKolumn <= 0)
+            {
+                throw new ArgumentException("Wymiary macierzy muszą być dodatnie.");
+            }
+
+            WymiarWierszy = wymiarWierszy;
+            WymiarKolumn = wymiarKolumn;
+            tablica = new T[wymiarWierszy, wymiarKolumn];
+        }
+
+        public T this[int wiersz, int kolumna]
+        {
+            get
+            {
+                if (wiersz < 0 || wiersz >= WymiarWierszy || kolumna < 0 || kolumna >= WymiarKolumn)
+                {
+                    throw new IndexOutOfRangeException("Indeks poza zakresem macierzy.");
+                }
+
+                return tablica[wiersz, kolumna];
+            }
+            set
+            {
+                if (wiersz < 0 || wiersz >= WymiarWierszy || kolumna < 0 || kolumna >= WymiarKolumn)
+                {
+                    throw new IndexOutOfRangeException("Indeks poza zakresem macierzy.");
+                }
+
+                tablica[wiersz, kolumna] = value;
+            }
+        }
+
+        public static bool operator ==(Macierz<T> m1, Macierz<T> m2)
+        {
+            if (ReferenceEquals(m1, m2)) return true;
+            if (m1 is null || m2 is null) return false;
+
+            if (m1.WymiarWierszy != m2.WymiarWierszy || m1.WymiarKolumn != m2.WymiarKolumn)
+                return false;
+
+            for (int i = 0; i < m1.WymiarWierszy; i++)
+            {
+                for (int j = 0; j < m1.WymiarKolumn; j++)
+                {
+                    if (!Equals(m1[i, j], m2[i, j]))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool operator !=(Macierz<T> m1, Macierz<T> m2)
+        {
+            return !(m1 == m2);
+        }
+
+        public bool Equals(Macierz<T> other)
+        {
+            return this == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Macierz<T> other)
+            {
+                return Equals(other);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return tablica.GetHashCode(); // Prosty sposób na zwrócenie hasha dla macierzy
+        }
+    }
---
+++ Zadnie5- ProgramowanieObiektowe/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Zadnie5__ProgramowanieObiektowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Przykład dla macierzy typu int
+            Macierz<int> macierz1 = new Macierz<int>(2, 2);
+            macierz1[0, 0] = 1;
+            macierz1[0, 1] = 2;
+            macierz1[1, 0] = 3;
+            macierz1[1, 1] = 4;
+
+            Macierz<int> macierz2 = new Macierz<int>(2, 2);
+            macierz2[0, 0] = 1;
+            macierz2[0, 1] = 2;
+            macierz2[1, 0] = 3;
+            macierz2[1, 1] = 4;
+
+            Macierz<int> macierz3 = new Macierz<int>(2, 2);
+            macierz3[0, 0] = 1;
+            macierz3[0, 1] = 2;
+            macierz3[1, 0] = 3;
+            macierz3[1, 1] = 5;
+
+            Console.WriteLine("Macierz 1:");
+            WyswietlMacierz(macierz1);
+
+            Console.WriteLine("\nMacierz 2:");
+            WyswietlMacierz(macierz2);
+
+            Console.WriteLine("\nMacierz 3:");
+            WyswietlMacierz(macierz3);
+
+            // Testowanie operatorów ==
+            Console.WriteLine("\nPorównanie macierzy 1 i 2:");
+            Console.WriteLine(macierz1 == macierz2); // powinno zwrócić true
+
+            Console.WriteLine("\nPorównanie macierzy 1 i 3:");
+            Console.WriteLine(macierz1 == macierz3); // powinno zwrócić false
+
+            // Testowanie operatora !=
+            Console.WriteLine("\nPorównanie macierzy 1 i 2:");
+            Console.WriteLine(macierz1 != macierz2); // powinno zwrócić false
+
+            Console.WriteLine("\nPorównanie macierzy 1 i 3:");
+            Console.WriteLine(macierz1 != macierz3); // powinno zwrócić true
+
+            Console.ReadKey();
+        }
+
+        static void WyswietlMacierz<T>(Macierz<T> macierz)
+        {
+            for (int i = 0; i < macierz.WymiarWierszy; i++)
+            {
+                for (int j = 0; j < macierz.WymiarKolumn; j++)
+                {
+                    Console.Write($"{macierz[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
